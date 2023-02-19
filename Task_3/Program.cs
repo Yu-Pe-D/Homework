0787db47de3ee1aed351905ab8aa753a117eea04
Yuri <loccckkky@yandex.ru>
2023-02-19 11:36:24 +0400

    Третье задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задайте номер четверти 1...4:");
+int a = int.Parse(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("x > 0; y > 0."); 
+}
+else if (a == 2)
+{
+  Console.WriteLine("x < 0; y > 0.");  
+}
+else if (a == 3)
+{
+Console.WriteLine("x < 0; y < 0.");    
+}
+else if (a == 4)
+{
+  Console.WriteLine("x > 0; y < 0.");    
+}
