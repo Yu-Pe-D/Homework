43a01510f67433a38dfed67e33f2350bb8befaee
Yuri <loccckkky@yandex.ru>
2023-02-19 10:09:08 +0400

    Первое задание

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿      Console.WriteLine ("Введите номер дня недели...");
+      int dayofweek = int.Parse (Console.ReadLine());
+      if (dayofweek == 6 ) Console.WriteLine ("Ответ: Суббота."); 
+      if (dayofweek == 7) Console.WriteLine ("Ответ: Воскресение.");
+      else if (dayofweek <= 5) Console.WriteLine ("Ответ: Будний день.");
+      else if (dayofweek > 7) Console.WriteLine ("Ошибка: В неделе всего 7 дней!");
+
